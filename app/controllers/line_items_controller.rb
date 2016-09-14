f65c3023c21b6781656e5f34f8ae34eb35783ebb
swaption2009 <swaption2009@gmail.com>
2016-09-14 16:31:11 -0700

    iteration I3: admin authorization and whitelist

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -8,8 +8,9 @@
 #---
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: :create
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authorize, only: :create
 
   # GET /line_items
   # GET /line_items.json
